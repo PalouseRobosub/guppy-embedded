2f795dbc4764972a8f59e3969d5f855f8748160a
guppy <guppyadrian@gmail.com>
2026-01-06 11:13:32 -0800

    feat: finished and tested the in-progress code

--- example/main.c
+++ example/main.c
@@ -1,6 +1,6 @@
 // This is the main program going on the Guppy microcontrollers
 //
-// I have no idea if this code works
+// This code listens to id 0x101 for float values, echoes the can frame on id 0x102, and outputs analog output on pin 20
 
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
@@ -9,25 +9,30 @@
 
 #include "../src/guppylib.h"
 
+#define PWM_PIN 20
+
 int main(void)
 {
     stdio_init_all();
     canbus_setup();
 
-    add_pwm_pin(pin_num); // set default duty cycle 1500 us
+    add_pwm_pin(PWM_PIN);
 
-    
-
-    can2040_msg msg = { 0 };
-    if (canbus_read(*msg)) // returns true if has message. Sets the *msg to the message
+    while (true)
     {
-        // TODO: id checking
+        struct can2040_msg msg = { 0 };
+        if (canbus_read(&msg)) // returns true if has message. Sets the &msg to the message
+        {
+            printf("Recieved frame\n");
+            if (msg.id != 0x101) continue;
 
+            float value = can_read_float(msg);
+            int us = throttle_to_pwm_us(value);
+            printf("pwm level (should be from 1100 to 1900): %d\n", us);
 
-        float value = can_read_float(msg);
-        int us = throttle_to_pwm_us(value);
-        pwm_write(pin_num, us);
+            pwm_write(PWM_PIN, us);
 
-        canbus_transmit_float(101, value); // echo the value
+            canbus_transmit_float(0x102, value); // echo the value
+        }
     }
 }
