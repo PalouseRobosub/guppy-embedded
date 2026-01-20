e2239b079510a0cf39b727202242de40717bc376
guppy <guppyadrian@gmail.com>
2026-01-20 03:23:05 +0000

    change to cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 // This is the main program going on the Guppy microcontrollers
 //
 // This code listens to id 0x101 for float values, echoes the can frame on id 0x102, and outputs analog output on pin 20
-
+extern "C" {
 #include "pico/stdlib.h"
 
 #include "guppy_lib.h"
 #include "modules/motor_controller.h"
+}
 
 int main(void)
 {
