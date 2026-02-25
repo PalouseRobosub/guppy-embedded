9a0b387a2ed97539a407d77ed8e777822e504990
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2026-02-25 01:46:46 +0000

    fix: All my problems were just a voltage difference the whole time (at 5V while trying to update it with 3.3V PIO) -- going back to the NeoPixel library because it's better

--- src/main.cpp
+++ src/main.cpp
@@ -1,149 +1,3 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-extern "C" {
-    #include <stdio.h>
-    #include <stdlib.h>
-
-    #include "pico/stdlib.h"
-    #include "hardware/pio.h"
-    #include "hardware/clocks.h"
-    #include "ws2812.pio.h"
-}
-
-/**
- * NOTE:
- *  Take into consideration if your WS2812 is a RGB or RGBW variant.
- *
- *  If it is RGBW, you need to set IS_RGBW to true and provide 4 bytes per
- *  pixel (Red, Green, Blue, White) and use urgbw_u32().
- *
- *  If it is RGB, set IS_RGBW to false and provide 3 bytes per pixel (Red,
- *  Green, Blue) and use urgb_u32().
- *
- *  When RGBW is used with urgb_u32(), the White channel will be ignored (off).
- *
- */
-#define IS_RGBW false
-#define NUM_PIXELS 144
-
-#ifdef PICO_DEFAULT_WS2812_PIN
-#define WS2812_PIN PICO_DEFAULT_WS2812_PIN
-#else
-// default to pin 2 if the board doesn't have a default WS2812 pin defined
-#define WS2812_PIN 20
-#endif
-
-// Check the pin is compatible with the platform
-#if WS2812_PIN >= NUM_BANK0_GPIOS
-#error Attempting to use a pin>=32 on a platform that does not support it
-#endif
-
-static inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb) {
-    pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
-}
-
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            (uint32_t) (b);
-}
-
-static inline uint32_t urgbw_u32(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            ((uint32_t) (w) << 24) |
-            (uint32_t) (b);
-}
-
-void pattern_snakes(PIO pio, uint sm, uint len, uint t) {
-    for (uint i = 0; i < len; ++i) {
-        uint x = (i + (t >> 1)) % 64;
-        if (x < 10)
-            put_pixel(pio, sm, urgb_u32(0xff, 0, 0));
-        else if (x >= 15 && x < 25)
-            put_pixel(pio, sm, urgb_u32(0, 0xff, 0));
-        else if (x >= 30 && x < 40)
-            put_pixel(pio, sm, urgb_u32(0, 0, 0xff));
-        else
-            put_pixel(pio, sm, 0);
-    }
-}
-
-void pattern_random(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand());
-}
-
-void pattern_sparkle(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand() % 16 ? 0 : 0xffffffff);
-}
-
-void pattern_greys(PIO pio, uint sm, uint len, uint t) {
-    uint max = 100; // let's not draw too much current!
-    t %= max;
-    for (uint i = 0; i < len; ++i) {
-        put_pixel(pio, sm, t * 0x10101);
-        if (++t >= max) t = 0;
-    }
-}
-
-typedef void (*pattern)(PIO pio, uint sm, uint len, uint t);
-const struct {
-    pattern pat;
-    const char *name;
-} pattern_table[] = {
-        {pattern_snakes,  "Snakes!"},
-        {pattern_random,  "Random data"},
-        {pattern_sparkle, "Sparkles"},
-        {pattern_greys,   "Greys"},
-};
-
-int main() {
-    //set_sys_clock_48();
-    stdio_init_all();
-    printf("WS2812 Smoke Test, using pin %d\n", WS2812_PIN);
-
-    // todo get free sm
-    PIO pio;
-    uint sm;
-    uint offset;
-
-    // This will find a free pio and state machine for our program and load it for us
-    // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
-    // so we will get a PIO instance suitable for addressing gpios >= 32 if needed and supported by the hardware
-    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_PIN, 1, true);
-    hard_assert(success);
-
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
-
-    int t = 0;
-    while (1) {
-        int pat = 0;//rand() % count_of(pattern_table);
-        int dir = (rand() >> 30) & 1 ? 1 : -1;
-        puts(pattern_table[pat].name);
-        puts(dir == 1 ? "(forward)" : "(backward)");
-        for (int i = 0; i < 1000; ++i) {
-            pattern_table[pat].pat(pio, sm, NUM_PIXELS, t);
-            sleep_ms(10);
-            t += dir;
-        }
-    }
-
-    // This will free resources and unload our program
-    pio_remove_program_and_unclaim_sm(&ws2812_program, pio, sm, offset);
-}
-/*
 // This is the main program going on the Guppy microcontrollers
 //
 // This code listens to id 0x101 for float values, echoes the can frame on id 0x102, and outputs analog output on pin 20
@@ -157,36 +11,26 @@ extern "C" {
 #include <iostream>
 #include "Adafruit_NeoPixel.hpp"
 
+#define NUM_LEDS 144
 
 int main(void)
 {
 
-    Adafruit_NeoPixel ledStrip(100, 16, NEO_GRB + NEO_KHZ800);
-    ledStrip.begin();
-    
+
 
     stdio_init_all();
     canbus_setup();
 
-    int brightnesses[100] = { 0 };
-    int inc = 0;
-
+    Adafruit_NeoPixel ledStrip(NUM_LEDS, 19, NEO_GRB + NEO_KHZ800);
+    ledStrip.begin();
     while(1)
     {
-        if (inc++ > 10)
+        for (int i = 0; i < NUM_LEDS; i++)
         {
-            inc = 0;
-            brightnesses[get_rand_32() % 100] = 100;
+            ledStrip.setPixelColor(i, ledStrip.Color(12, 0, 0));
+            ledStrip.show();
+            sleep_ms(10);
         }
-
-        for (int i = 0; i < 100; i++)
-        {
-            ledStrip.setPixelColor(i, ledStrip.Color(brightnesses[i], 0, 0));
-            if (brightnesses[i] > 0) brightnesses[i]--;
-        }
-        ledStrip.show();
-
-        sleep_ms(10);
     }
 
     sleep_ms(10000);
@@ -204,4 +48,3 @@ int main(void)
             break;
     }
 }
-*/
