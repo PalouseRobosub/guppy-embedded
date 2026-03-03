adaf23ce18970ccaa233aa5c937d5398a90cbef7
guppy <guppyadrian@gmail.com>
2026-03-03 21:35:36 +0000

    feat: if no motor signal is sent, reset motor value to 0

--- src/modules/board_motor.cpp
+++ src/modules/board_motor.cpp
@@ -8,16 +8,20 @@ extern "C" {
 #include "led.hpp"
 
 // TODO: untested pin numbers
-static const uint8_t pwm_pins[8] = { 16, 17, 18, 19, 20, 25, 26, 27 };
+#define NUM_PINS 8
+static const uint8_t pwm_pins[NUM_PINS] = { 16, 17, 18, 19, 20, 25, 26, 27 };
+static int last_updates[NUM_PINS]{};
 static const uint8_t led_pin = 28;
 static const uint16_t motor_board_id = 0x410;
 
-#define MOTOR_MULT 0.1
+#define MOTOR_MULT 0.5
+#define ALLOW_STALE_MOTORS true // if this is true it won't set stale motors to 0
 
 void board_motor_loop()
 {
-    for (int i = 0; i < sizeof(pwm_pins)/sizeof(uint8_t); i++) {
+    for (int i = 0; i < NUM_PINS; i++) {
         add_pwm_pin(pwm_pins[i]);
+        last_updates[i] = 0;
     }
 
     LEDController led_strip(led_pin);
@@ -27,33 +31,39 @@ void board_motor_loop()
         do_heartbeat(0x010);
         led_strip.tick();
 
+        const int cur_time = to_ms_since_boot(get_absolute_time());
+
         struct can2040_msg msg = { 0 };
         if (canbus_read(&msg)) // returns true if has message. Sets the &msg to the message
         {
             led_strip.update(msg);
-            #ifndef NDEBUG
-                printf("Recieved frame\n");
-            #endif
-            if (msg.id >= (motor_board_id+1) && msg.id <= (motor_board_id+8)) {
+            if (msg.id >= (motor_board_id+1) && msg.id <= (motor_board_id+NUM_PINS)) {
                 float value = can_read_float(msg) * MOTOR_MULT;
+                printf("float before clamp: %f\n", value);
                 if (value > MOTOR_MULT) value = MOTOR_MULT;
                 if (value < -MOTOR_MULT) value = -MOTOR_MULT;
                 int micro_seconds = throttle_to_pwm_us(value);
 
                 // NDEBUG should be added by CMAKE on release builds
-                #ifndef NDEBUG
-                    printf("received motor power for id %x\n", msg.id);
-                    printf("pwm level (should be from 1100 to 1900): %d\n", micro_seconds);
-                    printf("recieved float: %f\n", value);
-                #endif
+                printf("received motor power for id %x\n", msg.id);
+                printf("pwm level (should be from 1100 to 1900): %d\n", micro_seconds);
+                printf("recieved float: %f\n\n", value);
 
-                pwm_write(pwm_pins[msg.id - motor_board_id], micro_seconds);
-
-                // tech debt: 0x102 is a temp test id
-                #ifndef NDEBUG
-                    canbus_transmit_float(0x102, value); // echo the value
-                #endif
+                const int index = msg.id - motor_board_id - 1;
+                pwm_write(pwm_pins[index], micro_seconds);
+                last_updates[index] = cur_time;
             }
         }
+
+        #ifndef ALLOW_STALE_MOTORS
+        for (int i = 0; i < NUM_PINS; i++) // set stale motors to 0
+        {
+            if (cur_time - last_updates[i] > 500)
+            {
+                pwm_write(pwm_pins[i], 1500); // write 0
+                last_updates[i] = cur_time;
+            }
+        }
+        #endif
     }
 }
